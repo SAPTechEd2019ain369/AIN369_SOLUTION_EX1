373ec61ad9d46197342307aa3a75dcfe386f7070
roadmaptrackerpoc$roadmaptrackerpoc <david.stocker@sap.com>
2019-07-09 11:16:09 +0200

    d3 fix

--- webcomponent.js
+++ webcomponent.js
@@ -1,15 +1,29 @@
 (function()  {
-    
+    let d3Script = document.createElement('script');
+    d3Script.src = 'https://d3js.org/d3.v5.min.js';
+    d3Script.async = false;
+    document.head.appendChild(d3Script);
+
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
       <style>
       </style>
     `;
-    
-    class Gauge extends HTMLElement {
+
+    d3Script.onload = () => 
+
+    customElements.define('com-sap-teched-gauge-solution-exe1', class Gauge extends HTMLElement {
+
+
+        disconnectedCallback () {
+            // your cleanup code goes here
+            try{
+                document.head.removeChild(d3Script);
+            }
+            catch{}
+            }
     
         constructor() {
-
             super();
             //Constants
             if (!window._d3){
@@ -18,8 +32,10 @@
             
             this._shadowRoot = this.attachShadow({mode: 'open'});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._svgContainer;
         };
-       
-      }
-      customElements.define('com-sap-teched-gauge-solution-exe1', Gauge);
+    
+    
+    });
+        
     })();
